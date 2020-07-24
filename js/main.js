a104be208946e128e34729375eaca9b80e2420f2
kwarchal <kamilwarchal295@gmail.com>
2020-07-24 10:08:39 +0200

    dodanie opisu do paragrafu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let paragraf = document.getElementById('opis');
+var przycisk = document.querySelector('button');
+
+przycisk.onclick = function(){
+    paragraf.textContent = 'Lorem ipsum dolor sit..'
+}
+
